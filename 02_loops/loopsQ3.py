b82f0e64e23378c64aa7b5ba6c1cc003fef97201
Rishu Raj <143050346+Rishuraj1397@users.noreply.github.com>
2025-12-31 19:00:16 +0530

    ..

--- 02_loops/loopsQ3.py
+++ 02_loops/loopsQ3.py
@@ -1,4 +1,6 @@
 num = int(input("Enter a number: "))
 
 for j in range(1, 11):
+    if j == 5:
+        continue
     print(num, "x", j, "=", num * j)
---
+++ 02_loops/loopsQ4.py
@@ -0,0 +1,4 @@
+num = str(input("Enter a string: "))
+reversed_str = ""
+for char in num:
+    print(char)
